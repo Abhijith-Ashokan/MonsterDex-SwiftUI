9a8284d50fd4913884b10e4b3ee17ed51be77adf
AbhijithAshokanLTTS <141701989+AbhijithAshokanLTTS@users.noreply.github.com>
2023-09-05 15:43:34 +0530

    initial commit

---
+++ MonsterDex/MonsterDex/MonsterDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonsterDexApp.swift
+//  MonsterDex
+//
+//  Created by Abhijith on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MonsterDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
